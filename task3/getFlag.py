9ebad5f688be9cc880d5f7473560eaf5bb86882d
TomasButkevicius <tomas.butkevicius99@gmail.com>
2020-10-27 18:10:57 +0200

    getflag tweaked code

--- task3/getFlag.py
+++ task3/getFlag.py
@@ -1,23 +1,17 @@
 import requests
 import json
-def downloadFlag(country):
-    with open(country+'.png', 'wb') as handle:
-        if data.get(country) is None:
-            print("Flag not found. Deepest apologies.")
-        else:
+def downloadFlag(country):        
+    if data.get(country) is None:
+        print("Flag not found. Deepest apologies.")
+    else:
+        with open(country + '.png', 'wb') as handle:
             img_data = requests.get(data.get(country)).content
             handle.write(img_data)
     
 
-
-with open('token.txt', 'r') as file:
-    token = file.read()
-
 emojis_url = 'https://api.github.com/emojis'
 
-params = dict(username="tomasbutkevicius",password=token)
-
-resp = requests.get(url=emojis_url, params=params)
+resp = requests.get(url = emojis_url)
 data=resp.json()
 
 country = input("Enter country name of flag to download: ")
