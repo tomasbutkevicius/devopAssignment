5da723e2e0784d8bd567f23058249d7eaa40250c
TomasButkevicius <tomas.butkevicius99@gmail.com>
2020-10-27 17:57:52 +0200

    task3 complete user can download flag from git api

--- task3/getFlag.py
+++ task3/getFlag.py
@@ -1,6 +1,13 @@
 import requests
 import json
-
+def downloadFlag(country):
+    with open(country+'.png', 'wb') as handle:
+        if data.get(country) is None:
+            print("Flag not found. Deepest apologies.")
+        else:
+            img_data = requests.get(data.get(country)).content
+            handle.write(img_data)
+    
 
 
 with open('token.txt', 'r') as file:
@@ -13,8 +20,5 @@ params = dict(username="tomasbutkevicius",password=token)
 resp = requests.get(url=emojis_url, params=params)
 data=resp.json()
 
-
-print(data.get("lithuania"))
-
-
-    
+country = input("Enter country name of flag to download: ")
+downloadFlag(country.lower())
